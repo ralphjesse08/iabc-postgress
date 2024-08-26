d180098697633381a879f32bb6b56dd4d7941084
ralphjesse08 <ralphjessedomingo@gmail.com>
2024-08-26 18:55:55 +0800

    debugging 3

--- IABC/settings.py
+++ IABC/settings.py
@@ -28,10 +28,10 @@ environ.Env.read_env()
 
 # SECURITY WARNING: keep the secret key used in production secret!
 
-SECRET_KEY = env('SECRET_KEY')
+SECRET_KEY = 'django-insecure-+a*pyszpo6#i0auxx_+&3v!*1luy)q&(gb$32zf)dk2tue$yqz'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 #ALLOWED_HOSTS = ['iabcph.herokuapp.com']
 ALLOWED_HOSTS = ['*']
