2b0e5a65d852e0b9506a077d0a5386fdaad9fe45
ralphjesse08 <ralphjessedomingo@gmail.com>
2024-08-26 18:48:40 +0800

    debugging 2

--- IABC/settings.py
+++ IABC/settings.py
@@ -28,7 +28,7 @@ environ.Env.read_env()
 
 # SECURITY WARNING: keep the secret key used in production secret!
 
-SECRET_KEY = os.environ.get('SECRET_KEY')
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
